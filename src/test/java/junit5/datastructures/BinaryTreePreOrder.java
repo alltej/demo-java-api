ce19ccab5283f2f0b6c1be3d313f04c6931dace1
alltej <allan.tejano@gmail.com>
2018-04-18 16:29:02 -0400

    exercise

---
+++ src/test/java/junit5/datastructures/BinaryTreePreOrder.java
@@ -0,0 +1,46 @@
+package junit5.datastructures;
+
+/**
+ * @author Allan Tejano
+ * 4/18/2018
+ */
+public class BinaryTreePreOrder {
+
+    public static class TreeNode {
+
+        private TreeNode leftNode;
+        private TreeNode rightNode;
+
+        private Integer data;
+
+        public TreeNode( Integer data, TreeNode leftNode, TreeNode rightNode ) {
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
+            this.data = data;
+        }
+    }
+
+    public void preOrder( TreeNode root) {
+        if (root != null) {
+            System.out.println(root.data);
+            preOrder( root.leftNode );
+            preOrder( root.rightNode );
+        }
+    }
+
+    public static void main( String[] args ) {
+        TreeNode n10 = new TreeNode(10, null, null);
+        TreeNode n30 = new TreeNode(30, null, null);
+        TreeNode n20 = new TreeNode(20, n10, n30);
+
+
+        TreeNode n50 = new TreeNode(50, null, null);
+        TreeNode n70 = new TreeNode(70, null, null);
+        TreeNode n60 = new TreeNode(60, n50, n70);
+
+        TreeNode n40 = new TreeNode(40, n20, n60);
+
+        BinaryTreePreOrder app = new BinaryTreePreOrder();
+        app.preOrder(  n40);
+    }
+}
