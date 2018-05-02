9e2cedf7f150c286c0383e9f7c4143029dc82cb2
alltej <allan.tejano@gmail.com>
2018-05-02 14:29:34 -0400

    BST find lca

---
+++ src/main/java/com/alltej/apps/codmttl/CodMttlApp_180502.java
@@ -0,0 +1,44 @@
+package com.alltej.apps.codmttl;
+
+/**
+ * @author Allan Tejano
+ * 5/2/2018
+ */
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Given list of integers , find element n with max count
+ * example:
+ * list: [1, 3, 4, 4, 3, 2, 5, 6, 3, 4, 3]
+ * n: 3
+ */
+public class CodMttlApp_180502 {
+
+    private int findElementWithMaxCount( List<Integer> list ) {
+        Map<Integer, Long> map = list.stream()
+                .collect( Collectors.groupingBy( Integer::intValue, Collectors.counting() ) );
+        Map.Entry<Integer, Long> entry = map.entrySet().stream().collect( Collectors
+                .collectingAndThen( Collectors.reducing( ( e1, e2 ) -> e1.getValue() > e2.getValue() ? e1 : e2 ),
+                        Optional::get ) );
+        return entry.getKey();
+    }
+
+    @Test public void run_example() {
+        int n = findElementWithMaxCount( asList( 1, 3, 4, 4, 3, 2, 5, 6, 3, 4, 3 ) );
+        assertEquals( 3, n );
+    }
+
+    @Test public void another_example() {
+        int n = findElementWithMaxCount( asList( 1,1,1,1,1,11,1,1,1,1,12,2,2,2,2,2,3,3,3,3,3,1,1,1,1,1) );
+        assertEquals( 1, n );
+    }
+}
---
+++ src/main/java/com/alltej/apps/hdepot/FindLcaApp.java
@@ -0,0 +1,69 @@
+package com.alltej.apps.hdepot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Allan Tejano
+ * 5/2/2018
+ */
+public class FindLcaApp {
+
+
+    @Test public void lca_tests( ) {
+
+        Node node = new Node( 20 );
+        node.left = new Node( 10 );
+        node.right = new Node( 30 );
+
+        //10
+        node.left.left = new Node( 6 );
+        node.left.right = new Node( 12);
+
+        //6
+        node.left.left.left = new Node( 3);
+
+        //30
+        node.right.left = new Node( 24 );
+        node.right.right = new Node( 34 );
+
+        //24
+        node.right.left.left = new Node( 21 );
+
+        //34
+        node.right.right.right = new Node( 49 );
+
+        assertEquals( 10, findLca( node, 3, 12 ) );
+        assertEquals( 30, findLca( node, 21, 49 ) );
+    }
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node( int data ) {
+            this.data = data;
+        }
+
+
+    }
+
+    static  int findLca( final Node node, int a, int b ) {
+
+        Node root = node;
+        while ( root != null ) {
+            if (root.data > a && root.data > b) {
+                root = root.left;
+            } else if (root.data < a && root.data < b) {
+                root = root.right;
+            } else {
+                break;
+            }
+
+        }
+        return root.data;
+
+    }
+
+}
