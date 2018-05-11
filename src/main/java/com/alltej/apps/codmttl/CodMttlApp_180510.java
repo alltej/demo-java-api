2a8166eca68a7789523f9d73e178d475189848ce
alltej <allan.tejano@gmail.com>
2018-05-10 23:19:16 -0400

    added BfsPrintByDepthTemplate

--- src/main/java/com/alltej/apps/codmttl/CodMttlApp_180510.java
+++ src/main/java/com/alltej/apps/codmttl/CodMttlApp_180510.java
@@ -1,30 +1,31 @@
 package com.alltej.apps.codmttl;
 
-/**
- * @author Allan Tejano
- * 4/19/2018
- */
-
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Given list of integers , find element n with max count
- * example:
- * list: [1, 3, 4, 4, 3, 2, 5, 6, 3, 4, 3]
- * n: 3
+ * @author Allan Tejano
+ * 5/10/2018
  */
-public class CodMttlAppTemplate {
-
+public class CodMttlApp_180510 {
+    /**
+     * Given list of integers , find element n with max count
+     * example:
+     * list: [1, 3, 4, 4, 3, 2, 5, 6, 3, 4, 3]
+     * n: 3
+     */
     private int findElementWithMaxCount( List<Integer> list ) {
-        return 0;
+        Map<Integer, Long> map = list.stream().collect( groupingBy( Integer::intValue, counting() ) );
+        Map.Entry<Integer, Long> entry = map.entrySet().stream().collect(
+                collectingAndThen( reducing( ( e1, e2 ) -> e1.getValue() > e2.getValue() ? e1 : e2 ), Optional::get ) );
+        return entry.getKey();
     }
 
     @Test public void run_example() {
